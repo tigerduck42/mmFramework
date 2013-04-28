27b47e0e4c764846f287127b5c9e00b5ea90600c
Martin Mitterhauser <martin.mitterhauser@gmail.com>
2013-04-28 21:05:20 +0800

    * add licence

--- class/DB/SQLite.php
+++ class/DB/SQLite.php
@@ -1,4 +1,34 @@
 <?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Martin Mitterhauser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * @link https://github.com/tigerduck42/mmFramework
+ * @copyright 2013 Martin Mitterhauser
+ * @author Martin Mitterhauser <martin.mitterhauser at gmail.com>
+ * @package MmFramework
+ * @version 1.0
+ */
+
 class SQLite extends DBCore {
 
 	protected function _connect() {
@@ -12,10 +42,10 @@ class SQLite extends DBCore {
 		}
 		catch(exception $e) {
 			trigger_error('Connect Error ' . $e->getMessage(), E_USER_ERROR);
-		} 	
+		}
 	}
-	
-	
+
+
 	public function asfetch() {
 		if(!is_null($this->_resultHandle)) {
 			$this->_result = $this->_resultHandle->fetchArray();
@@ -24,7 +54,7 @@ class SQLite extends DBCore {
 			return FALSE;
 		}
 	}
-	
+
 	protected function _q($sql) {
 		return $this->_link->query($sql);
 	}
@@ -32,23 +62,23 @@ class SQLite extends DBCore {
 	protected function _escape($value) {
 		return $this->_link->escapeString($value);
 	}
-	
+
 	protected function _rows() {
 		return $this->_link->changes();
 	}
-	
+
 	protected function _affectedRows(){
 		return $this->_link->changes();
 	}
 	protected function _insertId() {
 		return $this->_link->lastInsertRowID();
-	}	
-	
+	}
+
 	protected function _errorNo() {
-		return $this->_link->lastErrorCode();	
+		return $this->_link->lastErrorCode();
 	}
 	protected function _errorMsg() {
-		return $this->_link->lastErrorMsg();	
+		return $this->_link->lastErrorMsg();
 	}
 }
 ?>
--- class/HTTP.php
+++ class/HTTP.php
@@ -1,11 +1,40 @@
 <?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Martin Mitterhauser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * @link https://github.com/tigerduck42/mmFramework
+ * @copyright 2013 Martin Mitterhauser
+ * @author Martin Mitterhauser <martin.mitterhauser at gmail.com>
+ * @package MmFramework
+ * @version 1.0
+ */
+
 class HTTP {
 
-
 	/*
-	 * GET 
+	 * GET
 	 */
-	 
+
 	static public function get($name) {
 		if(isset($_GET[$name])) {
 			return $_GET[$name];
@@ -13,12 +42,12 @@ class HTTP {
 		else {
 			return NULL;
 		}
-	} 
-	
+	}
+
 	/*
-	 * POST 
+	 * POST
 	 */
-	 
+
 	static public function post($name) {
 		if(isset($_POST[$name])) {
 			return $_POST[$name];
@@ -26,12 +55,12 @@ class HTTP {
 		else {
 			return NULL;
 		}
-	} 
-	
+	}
+
 	/*
-	 * Server 
+	 * Server
 	 */
-	 
+
 	static public function server($name) {
 		if(isset($_SERVER[$name])) {
 			return $_SERVER[$name];
@@ -39,9 +68,9 @@ class HTTP {
 		else {
 			return NULL;
 		}
-	} 
+	}
 
-	
+
 
 	/*
 	 * Cookie
@@ -57,19 +86,19 @@ class HTTP {
 		if(self::isCookieSet($name)) {
 			$cookie = $_COOKIE[$name];
 		}
-		
+
 		return $cookie;
 	}
-	
+
 	static public function deleteCookie($name) {
 		self::setCookie($name, "", time() - 3600);
 	}
-	
-	
+
+
 	static public function isCookieSet($name) {
 		return isset($_COOKIE[$name]);
 	}
-	
+
 	/*
 	 * Session
 	 */
@@ -78,30 +107,30 @@ class HTTP {
 		if(self::isSessionSet($name)) {
 			$value = $_SESSION[$name];
 		}
-		
+
 		return $value;
 	}
 
 	static public function isSessionSet($name) {
 		return isset($_SESSION[$name]);
 	}
-	
+
 	/*
 	 * Misc
 	 */
 	static public function me() {
   		return $_SERVER['SCRIPT_NAME'];
-  	} 
-	 
+  	}
+
 	static public function refresh() {
   		self::redirect(self::me());
   		exit;
   	}
-  	
+
   	static public function redirect($url) {
   		header("Location: " . $url);
   		exit;
-  	}	 
-	
+  	}
+
 }
 ?>
