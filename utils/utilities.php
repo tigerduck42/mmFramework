adf07cbec7404d81252f2ad2cd46e37466898068
Martin Mitterhauser <m.mitterhauser@ipanet.at>
2015-11-05 09:56:49 +0100

    * add helper to send debug emails

--- utils/utilities.php
+++ utils/utilities.php
@@ -25,6 +25,31 @@ function echo_nice($data, $return = FALSE)
   }
 }
 
+function email_nice($data)
+{
+  $config = fw\Config::getInstance();
+  $body = '';
+  if (is_array($data) || is_object($data)) {
+    $body .= print_r_nice($data, TRUE);
+  } else {
+    $body .= echo_nice($data, TRUE);
+  }
+
+
+  $mail = new fw\MyMailer();
+  $mail->From = $config->errorEmail;
+  $mail->FromName = "DebugNotify";
+  $mail->AddAddress($config->errorEmail, "WebAdmin");
+  $mail->Subject = "";
+  $mail->Subject .= "[DEBUG] " . fw\HTTP::hostname();
+  $mail->IsHTML();
+  $mail->setBody($body);
+
+  if (!$mail->Send()) {
+    trigger_error($mail->ErrorInfo, E_USER_ERROR);
+  }
+}
+
 function intNice($value)
 {
   if (!is_null($value)) {
