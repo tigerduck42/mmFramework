827dd5a099fc6e2f1ae21a1cef43b8be6c431eab
Martin Mitterhauser <m.mitterhauser@mytweak.at>
2016-06-03 14:36:53 +0200

    * add domain restriction to set cookie

--- class/HTTP.php
+++ class/HTTP.php
@@ -103,9 +103,9 @@ class HTTP
   /*
    * Cookie
    */
-  public static function setCookie($name, $value, $expire = 0, $path = '/')
+  public static function setCookie($name, $value, $expire = 0, $path = '/', $domain = NULL)
   {
-    if (!setcookie($name, $value, $expire, $path)) {
+    if (!setcookie($name, $value, $expire, $path, $domain)) {
       trigger_error("Cookie '" . $name . "' could not be set!", E_USER_ERROR);
     }
   }
